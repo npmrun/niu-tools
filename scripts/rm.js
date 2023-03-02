61901c1236b2dad9a7dfc35594f6e362f6789c12
npmrun <kk1549469775@gmail.com>
2023-03-02 16:40:02 +0800

    release

--- scripts/rm.js
+++ scripts/rm.js
@@ -2,6 +2,7 @@ import { deleteAsync } from 'del'
 
 const deletedFilePaths = await deleteAsync([
     'packages/**/dist/*',
+    '!packages/**/dist/node_modules',
     '!packages/**/dist/readme.md',
     '!packages/**/dist/CHANGELOG.md',
     '!packages/**/dist/package.json',
