ec2cc82c2821e2810822edd79b272120d68a363a
npmrun <1549469775@qq.com>
2023-03-10 12:45:44 +0800

    add test

---
+++ packages/core/common/array.test.ts
@@ -0,0 +1,7 @@
+import { random } from "./array"
+
+describe('sum', () => {
+    it('adds 1 + 2 to equal 3', () => {
+        expect(random([2,1]).length).toBe(2)
+    })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,8 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, "packages/niu-tools/**"],
-    include: ["packages/core/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    globals: true,
+    exclude: [...configDefaults.exclude],
+    include: ["packages/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
 });
