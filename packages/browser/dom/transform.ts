e27fdd0940a31dd99b6fd22145a9c0c7d3a7f63b
npmrun <1549469775@qq.com>
2023-10-16 15:17:21 +0800

    add transform

---
+++ packages/browser/dom/transform.ts
@@ -0,0 +1,12 @@
+// https://juejin.cn/post/7232209767593656378
+
+// https://juejin.cn/post/7212427549611016253
+// function str2dom(str: string) {
+//     const parser = new DOMParser();
+//     const htmlDoc = parser.parseFromString(str, 'text/html');
+//     return htmlDoc.body
+// }
+
+// function dom2str(dom: HTMLElement) {
+//     return dom.innerHTML
+// }
