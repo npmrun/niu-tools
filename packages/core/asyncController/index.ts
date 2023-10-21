5d7c6772d2da40baa40039818d9d58642342b37c
npmrun <1549469775@qq.com>
2023-10-21 11:06:12 +0800

    add

--- packages/core/asyncController/index.ts
+++ packages/core/asyncController/index.ts
@@ -1,14 +1,16 @@
-// @ts-nocheck
+
 
 /**
  * 任务执行状态枚举类
  * @type {{INIT: number, RUNNING: number, FINISH: number}}
  */
-const TaskStatusEnum = {
-    INIT: -1, //初始化
-    RUNNING: 0, //执行中
-    FINISH: 1 //已完成
+const enum TaskStatusEnum {
+    INIT = -1, //初始化
+    RUNNING = 0, //执行中
+    FINISH = 1 //已完成
 }
+
+type ITask = (end: Function) => void
 
 /**
  * 任务单元
@@ -17,45 +19,45 @@ const TaskStatusEnum = {
  * @param group   任务组对象
  * @constructor
  */
-function TaskUnit(taskId, task, group) {
-    this.taskId = taskId;  //任务唯一标识，暂时没用到
-    this.status = TaskStatusEnum.INIT; //执行状态：-1初始化  0执行中  1已完成
-    this.task = task; //任务内容
-    /**
-     * 执行任务内容
-     */
-    this.execute = () => {
-        this.status = TaskStatusEnum.RUNNING;
-        this.task(this.end);
+class TaskUnit {
+    taskId: number
+    status: TaskStatusEnum
+    task: ITask
+    private group: TaskGroup
+
+    constructor(taskId: number, task: ITask, group: any) {
+        this.taskId = taskId;  //任务唯一标识，暂时没用到
+        this.status = TaskStatusEnum.INIT; //执行状态：-1初始化  0执行中  1已完成
+        this.task = task; //任务内容
+        this.group = group; // 任务组对象
     }
-    /**
-     * 任务完成回调
-     */
-    this.end = () => {
+
+    execute() {
+        this.status = TaskStatusEnum.RUNNING;
+        const end = () => this.end()
+        this.task(end);
+    }
+
+    end() {
         if (this.status === TaskStatusEnum.RUNNING) {
             this.status = TaskStatusEnum.FINISH
-            group.check();
+            this.group.check();
         }
     }
 }
 
-/**
- * 任务组
- * @constructor
- */
-function TaskGroup() {
-    this.nextGroup = null;//下一个任务组
-    this.taskList = [];//任务列表
-    this.add = (task) => {
+class TaskGroup {
+    nextGroup: TaskGroup | null = null
+    taskList: TaskUnit[] = []
+
+    constructor() { }
+    add(task: TaskUnit) {
         this.taskList.push(task);
     }
-    this.setNextGroup = (group) => {
+    setNextGroup(group: TaskGroup) {
         this.nextGroup = group;
     }
-    /**
-     * 启动任务组
-     */
-    this.start = () => {
+    start() {
         for (let i = 0; i < this.taskList.length; i++) {
             const task = this.taskList[i];
             if (task.status === TaskStatusEnum.INIT) {
@@ -63,10 +65,7 @@ function TaskGroup() {
             }
         }
     }
-    /**
-     * 检查任务
-     */
-    this.check = () => {
+    check() {
         for (let i = 0; i < this.taskList.length; i++) {
             if (this.taskList[i].status !== TaskStatusEnum.FINISH) {
                 return; //发现还有任务没有执行完成
@@ -80,28 +79,18 @@ function TaskGroup() {
             this.nextGroup = null;//取消引用，让当前任务组尽快回收
         }
     }
-
 }
 
-interface asyncController {
-    new(): typeof asyncController;
-}
-
-/**
- * 异步函数控制器，
- * .and() 添加异步并行函数
- * .next() 添加同步串行函数
- */
-export function asyncController() {
-    this.queue = [];
-    this.nowTaskGroup = null;//当前任务组
-    this.startCount = 0;//加入任务数
+export class asyncController<T extends ITask> {
+    queue: TaskGroup[] = []
+    nowTaskGroup: TaskGroup | null = null
+    startCount: number = 0
 
     /**
      * 调用该函数表示添加并行任务
      * @param task 任务
      */
-    this.and = (task) => {
+    and(task: T) {
         if (this.nowTaskGroup == null) {
             this.nowTaskGroup = new TaskGroup();
         }
@@ -109,10 +98,10 @@ export function asyncController() {
         return this
     }
     /**
-     * 调用该函数表示添加串行任务
-     * @param task 任务
-     */
-    this.next = (task) => {
+    * 调用该函数表示添加串行任务
+    * @param task 任务
+    */
+    next(task: T) {
         if (this.nowTaskGroup != null) this.queue.push(this.nowTaskGroup);//防止上一个添加的任务是并行的
         this.nowTaskGroup = new TaskGroup();
         this.nowTaskGroup.add(new TaskUnit(++this.startCount, task, this.nowTaskGroup));
@@ -121,10 +110,10 @@ export function asyncController() {
         return this
     }
     /**
-     * 调用该函数表示任务添加完毕，开始执行任务
-     * @param endTask 任务全部结束后回调
-     */
-    this.finish = (endTask) => {
+    * 调用该函数表示任务添加完毕，开始执行任务
+    * @param endTask 任务全部结束后回调
+    */
+    finish = (endTask: T) => {
         if (this.nowTaskGroup != null) this.queue.push(this.nowTaskGroup);
         this.nowTaskGroup = new TaskGroup();
         this.nowTaskGroup.add(new TaskUnit(++this.startCount, endTask, this.nowTaskGroup));
